fa38cbae8f6fd7f93c4d47e3c92d1133724fbc3f
night-gold <braley.jo@gmail.com>
2019-02-25 22:55:39 +0100

    added statics file server

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -27,6 +30,11 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 func newRouter() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Recoverer)
+	//Statics file server
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "statics")
+	FileServer(r, "/statics", http.Dir(filesDir))
+	//Other routes
 	r.HandleFunc("/", homePage)
 	r.HandleFunc("/code", LanguageCode)
 	r.Mount("/{lang}/admin", adminRouter())
@@ -49,4 +57,24 @@ func handleRequests() {
 
 func main() {
 	handleRequests()
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
